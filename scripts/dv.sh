a8d9c3724f8ff31ad03ed132ce4f213a2b8eee13
ak1ra26 <41eckfmc19d@gmail.com>
2023-06-09 01:05:30 +0300

    Fixed counting

--- scripts/dv.sh
+++ scripts/dv.sh
@@ -4,26 +4,12 @@ init_i # initialize settings for interactive scripts
 config_file="$Dir_config/dv.cfg"
 dv_last="$Dir_cache/dv_last"
 
-[[ $1 == "-ff" ]] && { profile="0tpwhv3c.default-release"; shift; } || profile="ccrfprqf.default-beta"
-
-while [[ $# -gt 1 ]]; do
-    case $1 in
-        -ff)
-            browser="firefox"
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
-done
-
 echo "$@" > "$dv_last"
 sed -i 's/https/ https/g;s/ /\n/g' "$dv_last"
 sed -i '/^$/d' "$dv_last"
 sed -i -r 's/&pkey=.+//' "$dv_last"
 arrLinks=($(cat "$dv_last"))
-count=${#arrLinks[@]}
+count=1
 
 for i in ${arrLinks[@]}
 do
@@ -43,10 +29,10 @@ do
             ;;
     esac
     [ ! -d "$Dir_Data/Media/$dvpath" ] && mkdir -p "$Dir_Data/Media/$dvpath"
-    OK "Downloading $((count - ${#arrLinks[@]} + 1)) from from ${#arrLinks[@]} ${NC}"
-    yt-dlp -P "$Dir_Data/Media/$dvpath" --cookies-from-browser firefox:"/home/$USER/.mozilla/firefox/$profile/" --mark-watched --download-archive "$Dir_cache/$log_file" -f 'bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b' --embed-thumbnail --sub-langs en,uk --embed-subs --embed-metadata --no-progress $i -o '%(title)s [%(id)s].%(ext)s' --no-warnings --no-simulate
-    count=$((count - 1))
-    notify-send -a "DV" "Downloading" "Left: $count from ${#arrLinks[@]}"
+    OK "Downloading: $count from ${#arrLinks[@]}"
+    notify-send -a "DV" "Downloading" "$count from ${#arrLinks[@]}"
+    yt-dlp -P "$Dir_Data/Media/$dvpath" --cookies-from-browser firefox --mark-watched --download-archive "$Dir_cache/$log_file" -f 'bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b' --embed-thumbnail --sub-langs en,uk --embed-subs --embed-metadata --no-progress $i -o '%(title)s [%(id)s].%(ext)s' --no-warnings --no-simulate
+    count=$((count + 1))
 done
 OK "Finished!"
 notify-send -a "DV" Finished\! "${#arrLinks[@]} items done"
